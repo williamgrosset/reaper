0ddb953fdb06df23143efdedc984435ea59cc2b8
William Grosset <williamhgrosset@gmail.com>
2025-11-08 17:00:39 -0800

    Increase toast transparency

--- Modules/Widgets/DeathToast.lua
+++ Modules/Widgets/DeathToast.lua
@@ -36,7 +36,7 @@ local function createToast(self, playerLevel)
     tile = true, tileSize = 16, edgeSize = 16,
     insets = { left = 4, right = 4, top = 4, bottom = 4 }
   })
-  toast:SetBackdropColor(0, 0, 0, 1)
+  toast:SetBackdropColor(0, 0, 0, 0.7)
 
   local color = self.rarity:getColor()
   toast:SetBackdropBorderColor(color.red, color.green, color.blue)
